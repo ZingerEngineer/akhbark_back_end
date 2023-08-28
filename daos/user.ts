eaa990e32a8a7209e4cc3631458c944b2d425eb1
ZingerEngineer <ramyisez@gmail.com>
2023-08-29 00:51:57 +0300

    created new user daos

--- daos/user.ts
+++ daos/user.ts
@@ -6,7 +6,63 @@ export const createUser = (data: IUser) => {
   return user.save()
 }
 
-export const findOneUserByEmail = (email: string) => {
-  const user = User.findOne({ email })
-  return user
+export const findOneUserByEmail = async (email: string) => {
+  const user = await User.findOne({ email: email })
+  if (!user) throw new Error("User doesn't exist")
+  return user.toObject()
+}
+
+export const findOneUserByUserName = async (userName: string) => {
+  const user = await User.findOne({ userName: userName })
+  if (!user) throw new Error("User doesn't exist")
+  return user.toObject()
+}
+
+export const getUserPosts = async (userId: string) => {
+  const user = await User.findById(userId)
+  if (!user) throw new Error("User doesn't exist")
+  const userObject = user.toObject()
+  return userObject.posts
+}
+
+export const getUserRecentActivity = async (userId: string) => {
+  const user = await User.findById(userId)
+  if (!user) throw new Error("User doesn't exist")
+  const userObject = user.toObject()
+  return userObject.recentActivity
+}
+
+export const getUserFollowers = async (userId: string) => {
+  const user = await User.findById(userId)
+  if (!user) throw new Error("User doesn't exist")
+  const userObject = user.toObject()
+  return userObject.followers
+}
+
+export const getUserReports = async (userId: string) => {
+  const user = await User.findById(userId)
+  if (!user) throw new Error("User doesn't exist")
+  const userObject = user.toObject()
+  return userObject.reports
+}
+
+export const getUserComments = async (userId: string) => {
+  const user = await User.findById(userId)
+  if (!user) throw new Error("User doesn't exist")
+  const userObject = user.toObject()
+  return userObject.comments
+}
+
+export const getUserReaction = async (userId: string) => {
+  const user = await User.findById(userId)
+  if (!user) throw new Error("User doesn't exist")
+  const userObject = user.toObject()
+  return userObject.reactions
+}
+
+export const getUserAvatar = async (userId: string) => {
+  const user = await User.findById(userId)
+  if (!user) throw new Error("User doesn't exist")
+  const userObject = user.toObject()
+  return userObject.avatar
 }
