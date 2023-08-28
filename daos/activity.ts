a3b670d7c62b24d1c250aaddd75b1574eb294015
ZingerEngineer <ramyisez@gmail.com>
2023-08-29 00:52:42 +0300

    deleted unused dao

--- daos/activity.ts
+++ daos/activity.ts
@@ -1,12 +1,7 @@
 import { IActivity } from '../interfaces/global'
 import { Activity } from '../models/Activity'
 
-export const findManyActivities = () => {
-  return Activity.find()
-}
-
 export const createActivity = (data: IActivity) => {
   const activity = new Activity(data)
-
   return activity.save()
 }
