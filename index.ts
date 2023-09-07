2681ae6a65dca714a1caea398160515862c309a9
ZingerEngineer <ramyisez@gmail.com>
2023-09-07 18:35:15 +0300

    added CORS

--- index.ts
+++ index.ts
@@ -4,12 +4,22 @@ import express, { Request, Response, NextFunction } from 'express'
 import bodyParser = require('body-parser')
 import { dbConnection } from './services/dbConnection'
 import router from './routes/index'
+import cors from 'cors'
+const corsOptions = {
+  origin: 'http://localhost:3000'
+}
 
 const app = express()
 const port = process.env.PORT
 
+app.use(cors(corsOptions))
 app.use(bodyParser.json())
+
 app.use('/', router)
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send('Connected')
+  next()
+})
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
   dbConnection()
